603d3a1ee12fa0195480ea1264f205954876d032
Marc Juul <juul@sudomesh.org>
2015-03-31 17:56:58 -0700

    receive working and command line arg stuff added

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <stdarg.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
@@ -87,7 +88,31 @@ int handle_incoming(int sock, struct sockaddr_in* addr) {
   return -1;
 }
 
-int main() {
+// call with e.g. usage(argv[0], stdin) or usage(argv[0], stderr)
+void usage(char* command_name, FILE* out) {
+  char default_command_name[] = "notdhcpserver";
+  if(!command_name) {
+    command_name = (char*) &default_command_name;
+  }
+  fprintf(out, "Usage: %s [-v] ifname=ip [ifname2=ip2 ...]\n", command_name);
+  fprintf(out, "\n");
+  fprintf(out, "  -v: Enable verbose mode\n");
+  fprintf(out, "  -h: This help text\n");
+  fprintf(out, "\n");
+  fprintf(out, "For each interface where you want nodhcpserver to hand out an IP \"lease\"\n");
+  fprintf(out, "specify an interface+ip pair. E.g:\n");
+  fprintf(out, "\n");
+  fprintf(out, "  %s eth0.2=100.64.0.2 eth0.3=100.64.0.3\n", command_name);
+  fprintf(out, "\n");
+}
+
+void usagefail(char* command_name) {
+  fprintf(stderr, "Error: Missing required command-line arguments.\n\n");
+  usage(command_name, stderr);
+  return;
+}
+
+int main(int argc, char** argv) {
 
   fd_set fdset;
   int sock;
@@ -95,6 +120,33 @@ int main() {
   int broadcast_perm;
   int packetlen;
   int num_ready;
+  int sockmode;
+  int c;
+  int verbose = 0;
+  extern int optind;
+
+  if(argc <= 0) {
+    usagefail(NULL);
+    return 1;
+  }
+
+  while((c = getopt(argc, argv, "dh")) != -1) {
+    switch (c) { 
+    case 'v': 
+      printf("Verbose mode enabled\n");
+      verbose = 1; 
+      break; 
+    case 'h':
+      usage(argv[0], stdout);
+      return 0;
+    }
+  }
+
+  if(argc < optind+2) {
+    usagefail(argv[0]);
+    return;
+  }
+
   
   if((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
     perror("creating socket failed");
@@ -105,7 +157,18 @@ int main() {
     perror("binding to device failed");
     return 1;
   }
+
+  sockmode = fcntl(sock, F_GETFL, 0);
+  if(sockmode < 0) {
+    perror("error getting socket mode");
+    return 1;
+  }
   
+  if(fcntl(sock, F_SETFL, sockmode | O_NONBLOCK) < 0) {
+    perror("failed to set non-blocking mode for socket");
+    return 1;
+  }
+
   broadcast_perm = 1;
   if(setsockopt(sock, SOL_SOCKET, SO_BROADCAST, (void *) &broadcast_perm, sizeof(broadcast_perm)) < 0) {
     perror("setting broadcast permission on socket failed");
@@ -114,7 +177,7 @@ int main() {
 
   memset(&bind_addr, 0, sizeof(bind_addr));
   bind_addr.sin_family = AF_INET;
-  bind_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  bind_addr.sin_addr.s_addr = inet_addr("255.255.255.255");
   bind_addr.sin_port = htons(src_port);
 
   if(bind(sock, (struct sockaddr*) &bind_addr, sizeof(bind_addr)) < 0) {
@@ -124,16 +187,17 @@ int main() {
 
   printf("Listening for requests\n");
 
-  FD_ZERO(&fdset);
   for(;;) {
+    FD_ZERO(&fdset);
     FD_SET(sock, &fdset);
-    if((num_ready = select(sock, &fdset, NULL, NULL, NULL)) < 0) {;
+    if((num_ready = select(sock + 1, &fdset, NULL, NULL, NULL)) < 0) {
       if(errno == EINTR) {
         printf("huh?\n");
         continue;
       }
       perror("error during select");
     }
+    printf("select returned\n");
 
     if(FD_ISSET(sock, &fdset)) {
       printf("Request received!\n");
